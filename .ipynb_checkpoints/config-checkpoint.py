d9863537eec419a1ba4586a7b1ccc8ff079b2518
Gonzalo Gabriel Ramirez <gonzalogabrielramirez@buenosaires.gob.ar>
2023-10-19 13:44:51 -0300

    Finalizada la función add_companies

---
+++ .ipynb_checkpoints/config-checkpoint.py
@@ -0,0 +1,18 @@
+URL             = 'https://cas.buenosaires.gob.ar/acceso/login/login.zul?service=http://eut.gcba.gob.ar/gedo-web'
+USERNAME        = '20395531876'
+PASSWORD        = '78235514Ab?'
+
+COMPANIES       = {"TELECOM": "30639453738", 
+                  "TELMEX": "33695098419",
+                  "TELEFONICA": "30639453975",
+                  "TELECENTRO": "30640897267",
+                  "NSS": "30702652975",
+                  "BT": "30638203797",
+                  "GIGARED": "30663045179",
+                  "CIRION": "30626747171",
+                  "CPS": "30696850972",
+                  "ARSAT": "30709670413",
+                  "AYSA": "30709565075",
+                  "METROGAS": "30657863676",
+                  "EDESUR": "30655116512",
+                  "EDENOR": "30655116202"}
